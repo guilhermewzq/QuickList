0fa894c9d2ea5a6777e3bec281b34580f24f5295
guilhermewqz <gui7sampaio@gmail.com>
2025-05-31 15:28:22 -0700

    criacao da funcao para mostrar os itens da lista e criacao da funcao para marcar os itens como concluidos

--- main.js
+++ main.js
@@ -12,5 +12,32 @@ function addItem() {
 
     document.querySelector("#item").value = ""
 
-    console.log(itens)
+    showItensList()
 }
+
+function showItensList(){
+    const sectionList = document.querySelector(".list")
+
+    sectionList.innerHTML= ""
+
+    itens.sort((a, b) => Number(a.checked) - Number(b.checked));
+
+    itens.map((item, index) => {
+         sectionList.innerHTML += `<div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+                <button>
+                    <img src="./assets/trash-icon.svg" alt="trash-icon">
+                </button>
+            </div>`
+    })
+}
+    function checkItem(itemName) {
+        const item = itens.find((item) => item.name === itemName)
+        item.checked = !item.checked
+    }
