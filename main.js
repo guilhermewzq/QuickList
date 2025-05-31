dc517a00c5f0d3b14ddd8fef46a390248e07e938
guilhermewqz <gui7sampaio@gmail.com>
2025-05-31 15:36:39 -0700

    criacao da funcao para marcar os itens e criacao adicao do evendo de click no label

--- main.js
+++ main.js
@@ -25,11 +25,11 @@ function showItensList(){
     itens.map((item, index) => {
          sectionList.innerHTML += `<div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                    <input type="checkbox" name="list" id="item-${index}" ${item.checked === true ? "checked" : ""}>
                     <div class="custom-checkbox">
                         <img src="./assets/checked.svg" alt="checked">
                     </div>
-                    <label for="item-${index}">${item.name}</label>
+                    <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
                 </div>
                 <button>
                     <img src="./assets/trash-icon.svg" alt="trash-icon">
